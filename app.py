68568da89aa60b21f87b5780e8e671a2f68e6032
Sk_Husna <shaikhhusnaazeezullah@gmail.com>
2026-09-10 04:32:11 +0000

    adjusting files

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-import request 
+import requests 
 from flask import Flask,request
 from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 API_KEY = os.getenv("API_KEY")
+@app.route("/",methods="GET", "POST"])
